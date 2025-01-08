6bb9aa9a5ee12f2a192e9d89da4576340334af77
Nico <ne_ez@hotmail.com>
2025-01-08 14:23:09 +0100

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -4,12 +4,11 @@ data_reaction = [
         icon: "crossed-swords",
         subtitle: "Enemy leaves your reach",
         description: "You can rarely move heedlessly past your foes without putting yourself in danger",
-        reference: "PHB, pg. 195.",
+        reference: "PHB, pg. 371.",
         bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
-            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
+            "Trigger: enemy creature you can see leaves your reach using its action, its Bonus Action, its Reaction or one of its Speeds.",
+            "Make one melee attack with a weapon or an unarmed strike against the provoking creature.",
+            "The attack occurs right before the creature leaves your reach."
         ]
     },
     {
@@ -17,9 +16,9 @@ data_reaction = [
         icon: "stopwatch",
         subtitle: "Part of your Ready action",
         description: "Execute the reaction specified by your Ready action",
-        reference: "PHB, pg. 193.",
+        reference: "PHB, pg. 372-373.",
         bullets: [
-            "Trigger: specified by your <i>Ready</i> action."
+            "Trigger and Reaction: specified by your <i>Ready</i> action."
         ]
     },
     {
@@ -27,7 +26,7 @@ data_reaction = [
         icon: "magic-swirl",
         subtitle: "Cast time of 1 reaction",
         description: "Cast a spell with a casting time of 1 reaction",
-        reference: "PHB, pg. 192.",
+        reference: "PHB, pg. 235-238.",
         bullets: [
             "Trigger: specified by the spell.",
             "For further details, see the <i>Cast a spell</i> action."
