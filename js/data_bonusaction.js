1efed57f3d4efbf080e99828dedd21a491b8128b
Nico <ne_ez@hotmail.com>
2025-01-08 12:31:43 +0100

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -4,12 +4,11 @@ data_bonusaction = [
         icon: "crossed-swords",
         subtitle: "Use with the Attack action",
         description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
+        reference: "PHB, pgs. 213.",
         bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
+            "Only usable if you take the <i>Attack</i> action and attack with a light weapon.",
+            "Perform a single attack with a different light weapon.",
+            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative."
         ]
     },
     {
@@ -17,9 +16,9 @@ data_bonusaction = [
         icon: "magic-swirl",
         subtitle: "Cast time of 1 bonus action",
         description: "Cast a spell with a casting time of 1 bonus action",
-        reference: "PHB, pg. 192.",
+        reference: "PHB, pgs. 235-238, 363.",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+            "On a turn, you can expend only one spell slot to cast a spell. You can't, for example, cast a spell with a spell slot as your action and another one using your bonus action on the same turn.",
             "For further details, see the <i>Cast a spell</i> action."
         ]
     },
